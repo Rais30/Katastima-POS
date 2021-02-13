23a80417704f8cd26a438988041d32e1863b990b
RaisAryaguna <raisazaria30@gmail.com>
2021-02-13 15:07:54 +0700

    perubahan 2

--- src/screen/allRole/logIn/LogIn.js
+++ src/screen/allRole/logIn/LogIn.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {Component, useCallback} from 'react';
 import {
   Text,
@@ -9,6 +10,8 @@ import {
   ScrollView,
   ActivityIndicator,
   Linking,
+  Modal,
+  Alert,
 } from 'react-native';
 import styles from '../../../assets/style/boxAllRole/boxAutentikasi/boxAutentivikasi';
 
@@ -34,12 +37,76 @@ export class LogIn extends Component {
   constructor() {
     super();
     this.state = {
+      data: {},
       email: '',
       password: '',
       loading: false,
       url: 'http://sammpah.herokuapp.com/password/reset',
+      modal: false,
+      visible: true,
     };
   }
+  alert() {
+    Alert.alert(
+      '',
+      'Convirmation Email Anda',
+      [
+        {
+          text: 'Convirmation',
+          onPress: () => this.handlePress1(),
+        },
+      ],
+      {cancelable: false},
+    );
+  }
+  handlePress1 = async () => {
+    const supported = await Linking.canOpenURL(this.state.url);
+    if (supported) {
+      await Linking.openURL(this.state.url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${this.state.url}`);
+    }
+  };
+
+  LogIn() {
+    const {email, password} = this.state;
+    const url = `https://katastima-pos.herokuapp.com/api/auth/login`;
+    this.setState({loading: true});
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'applicetion/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((resjson) => {
+        console.log(resjson);
+        const {token} = resjson;
+        if (token) {
+          ToastAndroid.show(
+            ' Anda Telah Masuk',
+            ToastAndroid.SHORT,
+            ToastAndroid.CENTER,
+            // console.log(resJson),
+            AsyncStorage.setItem('token', token),
+            this.setState({loading: false}),
+            // this.props.navigation.navigate('Login'),
+          );
+        } else if (resjson.error) {
+          alert(resjson.error);
+          this.setState({loading: false});
+        } else {
+          console.log(error);
+          this.setState({loading: false});
+        }
+      });
+  }
+
   render() {
     return (
       <View style={styles.utama}>
@@ -52,33 +119,48 @@ export class LogIn extends Component {
           </View>
           <View style={styles.body}>
             <View style={{...styles.boxInput, marginTop: 80}}>
-              <TextInput style={styles.input} placeholder="Email" />
+              <TextInput
+                style={styles.input}
+                placeholder="Email"
+                onChangeText={(teks) => this.setState({email: teks})}
+              />
             </View>
             <View style={styles.boxInput}>
-              <TextInput style={styles.input} placeholder="Password" />
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                onChangeText={(teks) => this.setState({password: teks})}
+                secureTextEntry={this.state.visible}
+              />
             </View>
             <View style={styles.boxTeks}>
+              <View>
+                <Text style={styles.teksKlik}>RememberMy</Text>
+              </View>
               <View>
                 <OpenURLButton url={this.state.url}>
                   Lupa Password
                 </OpenURLButton>
               </View>
-              <View>
-                <Text
-                  onPress={() => this.props.navigation.navigate('Register')}
-                  style={styles.teksKlik}>
-                  Buat Akun
-                </Text>
-              </View>
             </View>
-            <TouchableNativeFeedback
-              onPress={() => this.setState({loading: !this.state.loading})}>
-              <View style={styles.tombol}>
+            <TouchableNativeFeedback onPress={() => this.LogIn()}>
+              <View
+                style={{
+                  ...styles.tombol,
+                  marginVertical: 10,
+                  marginHorizontal: 20,
+                }}>
                 {this.state.loading ? (
                   <ActivityIndicator size={25} color="white" />
                 ) : (
                   <Text style={styles.teksTombol}> Masuk </Text>
                 )}
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback
+              onPress={() => this.props.navigation.navigate('Register')}>
+              <View style={{...styles.tombol, marginVertical: 0}}>
+                <Text style={styles.teksTombol}> Daftar </Text>
               </View>
             </TouchableNativeFeedback>
           </View>
--- src/screen/allRole/register/Register.js
+++ src/screen/allRole/register/Register.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useCallback} from 'react';
 import {
   Text,
   View,
@@ -8,18 +8,110 @@ import {
   ToastAndroid,
   ScrollView,
   ActivityIndicator,
+  Linking,
+  Alert,
 } from 'react-native';
 import styles from '../../../assets/style/boxAllRole/boxAutentikasi/boxAutentivikasi';
+
+const OpenURLButton = ({url, children}) => {
+  const handlePress = useCallback(async () => {
+    const supported = await Linking.canOpenURL(url);
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${url}`);
+    }
+  }, [url]);
+
+  return (
+    <View style={styles.tomabol}>
+      <TouchableNativeFeedback onPress={handlePress}>
+        <Text style={{...styles.teksKlik, color: 'white'}}>{children}</Text>
+      </TouchableNativeFeedback>
+    </View>
+  );
+};
 
 export class LogIn extends Component {
   constructor() {
     super();
     this.state = {
+      name: '',
       email: '',
       password: '',
+      convirmasi: '',
       loading: false,
+      url: 'http://sammpah.herokuapp.com/password/reset',
+      visible: true,
+      visible1: true,
+      url1: 'https://katastima-pos.herokuapp.com/api/auth/register',
     };
   }
+  alert() {
+    Alert.alert(
+      '',
+      'Convirmation Email Anda',
+      [
+        {
+          text: 'Convirmation',
+          onPress: () => this.handlePress(),
+        },
+      ],
+      {cancelable: false},
+    );
+  }
+  handlePress = async () => {
+    const supported = await Linking.canOpenURL(this.state.url);
+    if (supported) {
+      await Linking.openURL(this.state.url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${this.state.url}`);
+    }
+  };
+
+  Register = () => {
+    const {name, email, password, convirmasi, url1} = this.state;
+
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+      password_confirmation: convirmasi,
+    };
+    this.setState({loading: true});
+    fetch(url1, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((respon) => respon.json())
+      .then((resJson) => {
+        console.log('apa ini respon', resJson);
+        if (resJson.status == 'success') {
+          ToastAndroid.show(
+            ' Anda Telah TerDaftar',
+            ToastAndroid.SHORT,
+            ToastAndroid.CENTER,
+            // console.log(resJson),
+            this.alert(),
+            this.setState({loading: false}),
+            this.props.navigation.navigate('LogIn'),
+          );
+        } else {
+          this.setState({loading: false});
+          console.log('error');
+          alert('Periksa kemabli data anda');
+        }
+      })
+      .catch((error) => {
+        this.setState({loading: false});
+        console.log('error is' + error);
+      });
+  };
+
   render() {
     return (
       <View style={styles.utama}>
@@ -32,27 +124,41 @@ export class LogIn extends Component {
           </View>
           <View style={styles.body}>
             <View style={{...styles.boxInput, marginTop: 80}}>
-              <TextInput style={styles.input} placeholder="Name" />
+              <TextInput
+                style={styles.input}
+                placeholder="Name"
+                onChangeText={(taks) => this.setState({name: taks})}
+              />
             </View>
             <View style={styles.boxInput}>
-              <TextInput style={styles.input} placeholder="Email" />
+              <TextInput
+                style={styles.input}
+                placeholder="Email"
+                onChangeText={(taks) => this.setState({email: taks})}
+              />
             </View>
             <View style={styles.boxInput}>
-              <TextInput style={styles.input} placeholder="Password" />
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                onChangeText={(taks) => this.setState({password: taks})}
+                secureTextEntry={this.state.visible}
+              />
             </View>
             <View style={styles.boxInput}>
               <TextInput
                 style={styles.input}
                 placeholder="Convirmation Password"
+                onChangeText={(taks) => this.setState({convirmasi: taks})}
+                secureTextEntry={this.state.visible1}
               />
             </View>
-            <TouchableNativeFeedback
-              onPress={() => this.setState({loading: !this.state.loading})}>
+            <TouchableNativeFeedback onPress={() => this.Register()}>
               <View style={styles.tombol}>
                 {this.state.loading ? (
                   <ActivityIndicator size={25} color="white" />
                 ) : (
-                  <Text style={styles.teksTombol}> Daftar </Text>
+                  <Text style={styles.teksTombol}>Daftar</Text>
                 )}
               </View>
             </TouchableNativeFeedback>
