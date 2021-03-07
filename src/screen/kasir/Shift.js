93844b1a48499dde864993af2dc755debf2635af
RaisAryaguna <raisazaria30@gmail.com>
2021-03-07 17:16:48 +0700

    perubahan 2.4.4.12

--- src/screen/kasir/Shift.js
+++ src/screen/kasir/Shift.js
@@ -16,7 +16,111 @@ export class Shift extends Component {
     this.state = {
       token: '',
       loading: false,
+      start_time: '',
+      end_time: '',
+      transaction_on_shift: '',
+      total_penjualan_on_shift: '',
+      duration: '0',
+      kode_kasir: '',
+      pesan: '',
     };
+  }
+  Mulai() {
+    const {token, pesan} = this.state;
+    const url = 'https://katastima-pos.herokuapp.com/api/kasir/shift/start';
+    console.log('Mulai shift');
+    this.setState({loading: true});
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'aplication/json',
+        'Content-Type': 'aplication/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((resJson) => {
+        const {status, data, message, error} = resJson;
+        if (status == 'success') {
+          console.log('Memulai shift (selamat bekerja)');
+          console.log('ini respon nya 1 :', resJson);
+          this.setState({
+            kode_kasir: data.kode_kasir,
+            start_time: data.start_time,
+            end_time: data.end_time,
+            transaction_on_shift: data.transaction_on_shift,
+          });
+          // ToastAndroid.show(
+          //   `${message}`,
+          //   ToastAndroid.SHORT,
+          //   ToastAndroid.CENTER,
+          // );
+          this.setState({loading: false});
+        } else if (status == 'failed') {
+          console.log('Cui belum waktu pulang ');
+          console.log('ini respon nya 2 :', resJson);
+
+          this.setState({pesan: JSON.stringify(error.message), kode_kasir: ''});
+          // ToastAndroid.show(
+          //   `${res}`,
+          //   ToastAndroid.SHORT,
+          //   ToastAndroid.CENTER,
+          // );
+          console.log(pesan);
+          this.setState({loading: false});
+        } else {
+          this.setState({loading: false});
+          console.log('ini respon nya : ', resJson);
+        }
+      })
+      .catch((error) => {
+        this.setState({loading: false});
+        console.log('ini adalah res pon error nya : ', error);
+      });
+  }
+  Akhir() {
+    const {token} = this.state;
+    const url = 'https://katastima-pos.herokuapp.com/api/kasir/shift/end';
+    console.log('Akhir shift');
+    this.setState({loading: true});
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'aplication/json',
+        'Content-Type': 'aplication/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((resJson) => {
+        const {status, data, message, error} = resJson;
+        if (status == 'success') {
+          console.log('Akhir shift (selamat Pulang bekerja)');
+          console.log('ini respon nya 1 :', resJson);
+          this.setState({
+            kode_kasir: data.kode_kasir,
+            start_time: data.start_time,
+            end_time: data.end_time,
+            transaction_on_shift: data.transaction_on_shift,
+            duration: data.duration,
+          });
+          ToastAndroid.show({message}, ToastAndroid.SHORT, ToastAndroid.CENTER);
+          this.setState({loading: false});
+        } else if (status == 'failed') {
+          console.log('Cui belum waktu pulang ');
+          console.log('ini respon nya 2 :', resJson);
+          this.setState({pesan: error.message[0]});
+          ToastAndroid.show({pesan}, ToastAndroid.SHORT, ToastAndroid.CENTER);
+          this.setState({loading: false});
+        } else {
+          this.setState({loading: false});
+          console.log('ini respon nya : ', resJson);
+        }
+      })
+      .catch((error) => {
+        this.setState({loading: false});
+        console.log('ini adalah res pon error nya : ', error);
+      });
   }
   componentDidMount() {
     AsyncStorage.getItem('token').then((token) => {
@@ -28,6 +132,26 @@ export class Shift extends Component {
       }
     });
   }
+  DataShift = () => {
+    const {
+      kode_kasir,
+      start_time,
+      end_time,
+      transaction_on_shift,
+      total_penjualan_on_shift,
+      duration,
+    } = this.state;
+    return (
+      <View>
+        <Text>Kode Kasir : {kode_kasir}</Text>
+        <Text>Mulai Shift : {start_time}</Text>
+        <Text>Akhir Shift : {end_time}</Text>
+        <Text>Transaksi : {transaction_on_shift}</Text>
+        <Text>Total Penjualan : {total_penjualan_on_shift}</Text>
+        <Text>Durasi Shift : {duration}</Text>
+      </View>
+    );
+  };
   render() {
     return (
       <View style={styles.utama}>
@@ -37,12 +161,12 @@ export class Shift extends Component {
             <ActivityIndicator size={35} color="red" />
           ) : (
             <View style={styles.Posisi}>
-              <TouchableNativeFeedback>
+              <TouchableNativeFeedback onPress={() => this.Mulai()}>
                 <View style={styles.tombol}>
                   <Text style={styles.taks}> Mulai Shift </Text>
                 </View>
               </TouchableNativeFeedback>
-              <TouchableNativeFeedback>
+              <TouchableNativeFeedback onPress={() => this.Akhir()}>
                 <View style={{...styles.tombol, backgroundColor: 'red'}}>
                   <Text style={styles.taks}> Akhiri Shift </Text>
                 </View>
@@ -50,7 +174,11 @@ export class Shift extends Component {
             </View>
           )}
           <View>
-            <Text> Data : </Text>
+            {this.state.kode_kasir == '' ? (
+              <View></View>
+            ) : (
+              <View>{this.DataShift()}</View>
+            )}
           </View>
         </ScrollView>
       </View>
